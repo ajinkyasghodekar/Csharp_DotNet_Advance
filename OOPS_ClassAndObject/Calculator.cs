87e9b5afaabb98d1bf6570fb11f5db9684eec4ae
ajinkyasghodekar <ajinkyasghodekar@gmail.com>
2023-04-30 12:50:27 +0530

    OOPS - Class and Object example

---
+++ OOPS_ClassAndObject/Calculator.cs
@@ -0,0 +1,18 @@
+public class Calculator
+    {
+        public void Add(int num1, int num2)
+        {
+            int result;
+            Console.WriteLine("This is addition: ");
+            result = num1 + num2;
+            Console.WriteLine(result);
+        }
+
+        public void Sub(int num1, int num2)
+        {
+            int result;
+            Console.WriteLine("This is substraction: ");
+            result = num1 - num2;
+            Console.WriteLine(result);
+        }
+    }
---
+++ OOPS_ClassAndObject/Program.cs
@@ -0,0 +1,10 @@
+﻿public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            calculator.Add(100, 100);
+            calculator.Sub(50, 40);
+        }        
+    }
