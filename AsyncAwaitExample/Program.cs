6b451598bde4d4b3d8f673f6569dc7f239c19255
ajinkyasghodekar <ajinkyasghodekar@gmail.com>
2023-04-30 12:31:53 +0530

    C# Async and Await example

---
+++ AsyncAwaitExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Methodm1();
+            Console.WriteLine("Main Method Thread");
+        }
+
+        public static async Task Methodm1()
+        {
+            await Task.Run(new Action (LongTask));
+            Console.WriteLine("New Methodm1 Thread");
+        }
+
+        public static void LongTask()
+        {
+            Thread.Sleep(20000);
+        }
+    }
+}
